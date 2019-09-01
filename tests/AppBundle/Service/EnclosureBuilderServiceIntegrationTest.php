f50a857cdf9112212fc0d715a625bf1aa86fa417
WENI HSU <hsu.weni@gmail.com>
2019-09-01 08:58:52 +0800

    22-Partial Mocking

--- tests/AppBundle/Service/EnclosureBuilderServiceIntegrationTest.php
+++ tests/AppBundle/Service/EnclosureBuilderServiceIntegrationTest.php
@@ -7,6 +7,7 @@ use AppBundle\Entity\Dinosaur;
 use AppBundle\Entity\Enclosure;
 use AppBundle\Entity\Security;
 use AppBundle\Service\EnclosureBuilderService;
+use DinosaurFactory;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
@@ -23,8 +24,20 @@ class EnclosureBuilderServiceIntegrationTest extends KernelTestCase
     public function testItBuildsEnclosureWithDefaultSpecifications()
     {
         /** @var EnclosureBuilderService $enclosureBuilderService */
-        $enclosureBuilderService = self::$kernel->getContainer()
-            ->get('test.'.EnclosureBuilderService::class);
+//        $enclosureBuilderService = self::$kernel->getContainer()
+//            ->get('test.'.EnclosureBuilderService::class);
+
+        $dinoFactory = $this->createMock(DinosaurFactory::class);
+        $dinoFactory->expects($this->any())
+            ->method('growFromSpecification')
+            ->willReturnCallback(function($spec) {
+                return new Dinosaur();
+            });
+
+        $enclosureBuilderService = new EnclosureBuilderService(
+            $this->getEntityManager(),
+            $dinoFactory
+        );
 
         $enclosureBuilderService->buildEnclosure();
 
